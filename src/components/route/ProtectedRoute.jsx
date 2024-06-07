f4dbbbb15b58f8c127223e47bbdafca050ab02da
codewithshumon <backlink.shumonkhan@gmail.com>
2024-06-07 21:28:13 +0600

    Completed

--- src/components/route/ProtectedRoute.jsx
+++ src/components/route/ProtectedRoute.jsx
@@ -6,14 +6,11 @@ import { AuthContext } from "../../context/AuthContext";
 const ProtectedRoute = ({ children }) => {
   const { token } = useContext(AuthContext);
 
-  console.log("[[token]]", token);
-
   if (token === null) {
     return <Navigate to="/" replace={true} />;
   }
 
-  const accessibleRoute =
-    token && token ? children : <Navigate to="/" replace={true} />;
+  const accessibleRoute = token ? children : <Navigate to="/" replace={true} />;
   return accessibleRoute;
 };
 
