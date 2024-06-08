73bd808aed896c482280a49ab7bc3237b62ad59e
codewithshumon <backlink.shumonkhan@gmail.com>
2024-06-08 20:44:55 +0600

    Fixed

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://shumon-replymind.onrender.com";
+export const BASE_URL = "https://shumon-replymind.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
